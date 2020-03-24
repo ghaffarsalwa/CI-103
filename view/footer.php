4284bd52d13579404862f126175c8939aaa801ec
Salwa Ghaffar <sg3628@drexel.edu>
2020-03-24 12:26:28 -0400

    Add files via upload

---
+++ view/footer.php
@@ -0,0 +1,9 @@
+        <div id="footer">
+                       
+            <p class="copyright">
+                &copy; <?php echo date("Y"); ?> Mariomart, Inc.
+            </p>
+        </div>
+    <!-- end page -->
+    </body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <!-- the head section -->
+    <head>
+        <title>Welcome to Mariomart</title>
+        <link rel="stylesheet" type="text/css"
+              href="../styles.css" />
+    </head>
+
+    <div id="topBarDiv">
+    <img id="topLogo" src="../images/pageImages/Logo.png">
+    <div id="logoText">MarioMart</div>
+    <a id="logout" href="../login.php">Log out </a>
+    </div>
+    <!-- the body section -->
+    <body>
+
+    <div id="page">
+        <div id="header">
+            
+        </div>
+       
